3a4ec347ab1d3ee16f0934ae35cef6960942745e
Benjamin Schaefer <benjaminschaefer@mail.de>
2016-04-03 17:03:11 +0200

    inserted cache saving of values

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
+
+
 (function(){
-  var app = angular.module("tasks", []);
+  var app = angular.module("tasks", ["angular-cache"]);
 
-  app.controller("TaskController", function(){
-    
+  app.controller("TaskController", function(CacheFactory){
 
-    this.tasks = tasks;
-    this.new = {};
-    // todo: get from browser cache / cookies
+    if (!CacheFactory.get('taskCache')) {
+      CacheFactory.createCache('taskCache', {
+        deleteOnExpire: 'none',
+        storageMode: 'localStorage'
+      });
+    }
+    taskCache = CacheFactory.get('taskCache');
+
+    if(taskCache.values().length === 0) {
+      createDefaultValues();
+    }
+
+    this.tasks = taskCache.values();
+    this.new = {};        // dummy for new object
 
     this.add = function() {
       this.new.date_of_creation = new Date();
       this.tasks.push(this.new);
+      taskCache.put('/tasks/'+taskCache.values().length, this.new)
       this.new = {};
     }
-
 
     this.edit = function() {
       // body...
@@ -42,4 +54,44 @@
     date_of_creation: "2016-04-02",
   }
   ];
+
+  var createDefaultValues = function() {
+    console.log("createDefaultValues");
+    tasks.forEach(function(task, index, array){
+      taskCache.put('/tasks/'+index, task);
+    });
+  }
+      
 })();
+
+
+
+
+/*
+
+
+
+
+angular.module('myApp', ['angular-cache'])
+  .config(function (CacheFactoryProvider) {
+    angular.extend(CacheFactoryProvider.defaults, { maxAge: 15 * 60 * 1000 });
+  })
+  .service('BookService', function (CacheFactory, $http) {
+    if (!CacheFactory.get('bookCache')) {
+      // or CacheFactory('bookCache', { ... });
+      CacheFactory.createCache('bookCache', {
+        deleteOnExpire: 'aggressive',
+        recycleFreq: 60000
+      });
+    }
+
+    var bookCache = CacheFactory.get('bookCache');
+
+    return {
+      findBookById: function (id) {
+        return $http.get('/api/books/' + id, { cache: bookCache });
+      }
+    };
+  });
+
+  */
